fa1db45fedd9cf46d7fa1d77f8870eead375b749
Mihai Renea <mihai.renea@ml-pa.com>
2024-04-10 15:46:57 +0200

    Added LSP support for bash

--- init.lua
+++ init.lua
@@ -639,6 +639,7 @@ require('mason-lspconfig').setup()
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
 local servers = {
+  bashls = {},
   clangd = {
     cmd = { "clangd", "--header-insertion=never", "--offset-encoding=utf-16" }
   },
